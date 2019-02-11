f6894be4c2960896c17a675d74cc48b0ac157af0
Robert Smith <ert135@gmail.com>
2019-02-11 20:12:51 +0000

    added files

---
+++ Assets/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float xSpeed = 4f;
+    [SerializeField] float ySpeed = 4f;
+    [SerializeField] float xConstraint = 6f;
+    [SerializeField] float yConstraint = 6f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -30f;
+    [SerializeField] float positionYawFactor = 4.5f;
+    [SerializeField] float controlRollFactor = -20f;
+    float xOffsetValue;
+    float yOffsetValue;
+    float xThrow;
+    float yThrow;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    void processTranslation()
+    {
+        this.xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        this.xOffsetValue = this.xThrow * this.xSpeed * Time.deltaTime;
+        float rawNewXPosition = transform.localPosition.x + xOffsetValue;
+        float finalPositionX = Mathf.Clamp(rawNewXPosition, -xConstraint, xConstraint);
+
+        transform.localPosition = new Vector3(finalPositionX, transform.localPosition.y, transform.localPosition.z);
+
+        this.yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        this.yOffsetValue = this.yThrow * this.ySpeed * Time.deltaTime;
+        float rawNewYPosition = transform.localPosition.y + yOffsetValue;
+        float finalPositionY = Mathf.Clamp(rawNewYPosition, -yConstraint, yConstraint);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, finalPositionY, transform.localPosition.z);
+    }
+
+    void processRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * this.positionPitchFactor;
+        float pitchDueToControlThrow = this.yThrow * this.controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * this.positionYawFactor;
+        float roll = this.xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.processTranslation();
+        this.processRotation();
+    }
+}
---
+++ Assets/musicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class musicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+        Invoke("loadFirstScene", 3f);
+    }
+	
+
+    void loadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
